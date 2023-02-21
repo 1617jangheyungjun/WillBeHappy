e5bba42cdea55716d3d1acc2126476598cc48c1b
jangh <jangh4139@gmail.com>
2023-02-22 01:27:07 +0900

    점프대 변경필요해 보임 여러번 점프 불가하게 바꾸기 metrial은 새로운 게임마다 적용되어서 불가능 하위객체 만들어서 인스턴스화 한뒤에 밟으면 지웠다가 일정 시간후 다시 부르기 필요해 보임 '

--- WillBeHappy/Assets/JumpFlower_script/JumpFlower.cs
+++ WillBeHappy/Assets/JumpFlower_script/JumpFlower.cs
@@ -7,26 +7,64 @@ public class JumpFlower : MonoBehaviour
     [SerializeField] float MaxJumpingSpeed = 30f;
 
     [SerializeField] float  SmallestSpeed = 30f;
+    BoxCollider2D myboxcollider;
+
+    float InitialBounce = 1.2f;
+
+    float jumpDelay = 5f;
+    float InitialJumpDelay;
+
+    bool dojump = false;
+    float SmallestSpeed2;
     // Start is called before the first frame update
     void Start() 
     {
-        Debug.Log("실행됨");    
+        InitialJumpDelay = jumpDelay;
+        Debug.Log("실행됨");   
+    }
+
+    void Update() 
+    {
+        Debug.Log(InitialBounce);
+        if(dojump)
+        {
+            jumpDelay -= Time.deltaTime;
+            Debug.Log(jumpDelay);
+            if(jumpDelay < 0)
+            {
+                jumpDelay = InitialJumpDelay;
+                dojump = false;
+            }
+        }    
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        SmallestSpeed2 = other.GetComponent<Rigidbody2D>().velocity.y;
     }
     void OnCollisionEnter2D(Collision2D other)
     {   
         Debug.Log("점프력"+other.gameObject.GetComponent<Rigidbody2D>().velocity.y);
         Rigidbody2D otherrigid = other.gameObject.GetComponent<Rigidbody2D>();
-        if(other.gameObject.tag == "Player")
+        
+        if(other.gameObject.tag == "Player" & !dojump)
         {
-            if(otherrigid.velocity.y > MaxJumpingSpeed)
+            dojump = true;
+            if(SmallestSpeed2 > MaxJumpingSpeed)
             {
                 otherrigid.velocity = new Vector2 (0, MaxJumpingSpeed);
             }
 
-            if(otherrigid.velocity.y < SmallestSpeed)
+            else if(SmallestSpeed2 < SmallestSpeed)
             {
                 otherrigid.velocity = new Vector2 (0, SmallestSpeed);
             }
+
+            else
+            {   
+                otherrigid.velocity = new Vector2 (0, -SmallestSpeed2 * 1.2f);
+            }
+            Debug.Log(SmallestSpeed2 + "꿝");
         }
+        
     }    
 }
